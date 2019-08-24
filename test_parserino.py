36b7eeb8d756a797de238cf494826bb098b76942
megalex97 <megalex97@hotmail.com>
2019-08-24 18:06:20 +0300

    added pep 8 changes

--- test_parserino.py
+++ test_parserino.py
@@ -5,11 +5,9 @@ def test_getPlace():
     question = 'Grand py, ou se trouve la cathedrale de paris ?'
     question2 = 'Grand py, ou se trouve cathedrale paris ?'
     question3 = 'le louvre'
-    question4 =  "Grand py, ou se trouve l'adresse d'openclassroom ?"
+    question4 = "Grand py, ou se trouve l'adresse d'openclassroom ?"
     assert parserino.getPlace(question) == 'cathedrale de paris'
-    assert parserino.getPlace(question2) == 'Grand py, ou se trouve cathedrale paris'
+    assert parserino.getPlace(question2) == ('Grand py, ou se '
+                                             'trouve cathedrale paris')
     assert parserino.getPlace(question3) == 'louvre'
     assert parserino.getPlace(question4) == 'de openclassroom'
-
-
-
