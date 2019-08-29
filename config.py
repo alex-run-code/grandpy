50adfe358f73d1d841eb6d4fae1cb870b0da8a46
megalex97 <megalex97@hotmail.com>
2019-08-29 18:40:21 +0300

    I added GOOGLE_API_KEY_MAPS and GOOGLE_API_KEY_GEOCODING in config.py and changed GOOGLEAPIKEYMAPS in app.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+GOOGLE_API_KEY_MAPS = os.environ.get('GOOGLE_API_KEY_MAPS')
+GOOGLE_API_KEY_GEOCODING = os.environ.get('GOOGLE_API_KEY_GEOCODING')
 
 try:
     from config_local import *
